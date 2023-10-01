0fbff106d6cee74b34dbe395d12dc72e52ebfbe7
Tar0k <73.tarok@gmail.com>
2023-10-01 15:22:19 +0400

    Init

--- OOP_Seminar3/LearningGroupService.cs
+++ OOP_Seminar3/LearningGroupService.cs
@@ -1,6 +1,17 @@
-﻿namespace OOP_Seminar3;
+﻿
 
-public class LearningGroupService
+namespace OOP_Seminar3;
+
+public class LearningGroupService(List<Group> groups)
 {
-    
+    public void RemoveByName(string name)
+    {
+        foreach (var group in groups)
+        {
+            foreach (var student in group)
+            {
+                if (student.FullName == name) group.Remove();
+            }
+        }
+    }
 }
--- OOP_Seminar3/StudentComparator.cs
+++ OOP_Seminar3/StudentComparator.cs
@@ -1,6 +1,14 @@
 ﻿namespace OOP_Seminar3;
 
-public class StudentComparator
+public class StudentComparator: IComparer<Student>
 {
-    
+    public int Compare(Student? student1, Student? student2)
+    {
+        if(student1 is null || student2 is null) 
+            throw new ArgumentException("Некорректное значение параметра");
+
+        if (student1.StudentNumber > student2.StudentNumber) return 1;
+        if (student1.StudentNumber < student2.StudentNumber) return -1;
+        return 0;
+    }
 }
